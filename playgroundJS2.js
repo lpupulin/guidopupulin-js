5a18f6f827cab468b3d757945def79faffc4e661
lpupulin <45633678+lpupulin@users.noreply.github.com>
2026-02-12 11:42:12 +0100

    Update playgroundJS2.js

--- playgroundJS2.js
+++ playgroundJS2.js
@@ -4,7 +4,7 @@ if (window.innerWidth > 900) {
   var img = new Image();
   var clearCanvas;
   var fader;
-  img.src = "https://www.lucapupulin.com/wp-content/uploads/2021/01/brush1r.png";
+  img.src = "https://www.lucapupulin.com/wp-content/uploads/2024/03/brush1g.png";
   img.width = 360;
 
   function distanceBetween(point1, point2) {
